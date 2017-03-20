a05b5632af005d22eda63ac2ff01e56e775eddb5
Jarno Lehtinen <lehtinen@sci.fi>
2017-03-20 22:05:53 +0200

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -4,10 +4,8 @@ dofile("ws2811.lua");
 
 init_wifi_mqtt( function(conn, topic, input) 
     if input == "ON" then 
-      update_ws2811(0.5) 
+      strip.rainbow()
     else 
-      update_ws2811(0.0) 
+      strip.off()
     end 
   end)
-  
-  
