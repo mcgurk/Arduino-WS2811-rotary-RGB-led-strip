19c014cc4acf52b2f42905ff3f7b59af6ea0c2b4
Jarno Lehtinen <lehtinen@sci.fi>
2017-03-20 23:06:13 +0200

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -1,11 +1,20 @@
+--mytimer = tmr.create():alarm(1000, 1, function() print(wifi.sta.getip()) end)
+
+-- Wifi credentials
+wifi.setmode(wifi.STATION)
+--wifi.sta.config("ssid","password")
+
 dofile("mqtt.lua");
 
 dofile("ws2811.lua");
 
-init_wifi_mqtt( function(conn, topic, input) 
+strip.off()
+
+init_mqtt( function(conn, topic, input) 
     if input == "ON" then 
       strip.rainbow()
     else 
       strip.off()
     end 
   end)
+
