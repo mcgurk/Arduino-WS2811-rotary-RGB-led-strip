6276f443892ef1d666bfc457e1b769fb186343ee
Jarno Lehtinen <lehtinen@sci.fi>
2017-03-20 20:13:08 +0200

    Create init.lua

---
+++ lua/init.lua
@@ -0,0 +1,25 @@
+-- init.lua
+
+function abortInit()
+  abort = false
+  print("Press ENTER to abort startup")
+  uart.on("data", "\r", abortTest, 0)
+  tmr.alarm(0,5000,0,startup)
+end
+    
+function abortTest(data)
+  abort = true
+  uart.on("data")
+end
+
+function startup()
+  uart.on("data")
+  if abort == true then
+    print("startup aborted")
+    return
+  end
+  print("in startup")
+  dofile("main.lua")
+end
+
+tmr.alarm(0, 1000, 0, abortInit)
