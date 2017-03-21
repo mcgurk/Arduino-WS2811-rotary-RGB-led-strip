5e603021a73e3c37bf055aae4c7e62bc80755be1
Jarno Lehtinen <lehtinen@sci.fi>
2017-03-21 19:40:44 +0200

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -1,5 +1,7 @@
 --mytimer = tmr.create():alarm(1000, 1, function() print(wifi.sta.getip()) end)
 
+
+
 -- Wifi credentials
 wifi.setmode(wifi.STATION)
 --wifi.sta.config("ssid","password")
@@ -10,11 +12,27 @@ dofile("ws2811.lua");
 
 strip.off()
 
-init_mqtt( function(conn, topic, input) 
-    if input == "ON" then 
+init_mqtt( function(conn, topic, input)
+    print("mqtt callback")
+    if string.upper(input) == "ON" then 
       strip.rainbow()
-    else 
+    elseif string.upper(input) == "OFF" then 
       strip.off()
+    elseif string.upper(input) == "KITT" then
+      strip.kitt(0.8, 20, 255, 0, 0)
+    elseif string.upper(input) == "RAINBOW" then
+      strip.rainbow()
+    else
+      --print("execute mqtt input string (type: " .. type(input) .. ")")
+      --loadstring(input)
+      print("mqtt input: " .. input)
+      print("mqtt input type: " .. type(input))
+      a = tonumber(input)
+      --print(a)
+      print("mqtt input tonumber: " .. a)
+      print("mqtt input tonumber type: " .. type(a))
+      --strip.set(a)
+      strip.location(a)
     end 
   end)
 
