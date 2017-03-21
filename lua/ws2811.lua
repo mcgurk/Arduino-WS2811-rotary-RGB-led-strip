0ed900b1a6b0d5992f377a0e7080077b73677a6c
Jarno Lehtinen <lehtinen@sci.fi>
2017-03-21 19:41:07 +0200

    Update ws2811.lua

--- lua/ws2811.lua
+++ lua/ws2811.lua
@@ -9,18 +9,26 @@ PIXELCOUNT = 50
 local buffer = ws2812.newBuffer(PIXELCOUNT, 3)
 local i = 1
 local d = true
+local frame = 1
+local location = 1
+
+local function striptimer(callback, speed)
+  if not speed then speed = 100 end
+  tmr.alarm(2, speed, 1, function()
+    callback()
+    frame = frame + 1
+    ws2812.write(buffer)
+  end)
+end
 
 function strip.rainbow(brightness, speed)
   if not brightness then brightness = 0.5 end
-  if not speed then speed = 100 end
   for pixel = 0, PIXELCOUNT-1 do
     buffer:set(pixel+1, hsvToRgb(pixel/PIXELCOUNT, 1.0, brightness))
   end
-  tmr.alarm(2, speed, 1, function()
-    --i = i + 1
+  striptimer(function()
     buffer:shift(1, ws2812.SHIFT_CIRCULAR)
-    ws2812.write(buffer)
-  end)
+  end, speed)
 end
 
 function strip.color(r, g, b)
@@ -44,26 +52,43 @@ end
 function strip.fade(factor, speed)
   if not factor then factor = 0.5 end
   if not speed then speed = 100 end
-  tmr.alarm(2, speed, 1, function()
+  striptimer(function()
     buffer:mix(factor * 255, buffer)
-    ws2812.write(buffer)
-  end)
+  end, speed)
 end
 
-function strip.kitt(fadefactor, speed, r, g, b)
-  if not fadefactor then fadefactor = 0.5 end
+function strip.point(factor, speed)
+  if not factor then factor = 0.5 end
   if not speed then speed = 100 end
-  if not r then r = 255 g = 0 b = 0 end
+  striptimer(function()
+    buffer:mix(factor * 255, buffer)
+    strip.set(location, 255, 0, 0)
+  end, speed)
+end
+
+function strip.location(newlocation)
+  location = newlocation
+end
+
+function strip.kitt(factor, speed, r, g, b)
+  if not factor then factor = 0.5 end
+  --if not r then r = 255 g = 0 b = 0 end
   d = true
   i = 1
-  tmr.alarm(2, speed, 1, function()
-    buffer:mix(fadefactor * 255, buffer)
-    buffer:set(i, r, g, b)
+  striptimer(function()
+    buffer:mix(factor * 255, buffer)
+    --buffer:set(i, r, g, b)
+    --buffer:set(i, hsvToRgb((frame%1024)/1024.0, 1.0, 1.0))
+    if not r then 
+      strip.set(i, hsvToRgb((frame%1024)/1024.0, 1.0, 1.0))
+    else
+      strip.set(i, r, g, b)
+    end
     if d then i = i + 1 else i = i - 1 end
     if i < 1 then d = true i = 1 end
     if i > PIXELCOUNT then d = false i = PIXELCOUNT end
-    ws2812.write(buffer)
-  end)
+  end, speed)
 end
+
 
 ws2812.init()
