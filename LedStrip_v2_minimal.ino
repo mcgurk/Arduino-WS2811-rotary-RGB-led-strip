71a61815c3b842db3ce641263b727347e4693c1b
Jarno Lehtinen <lehtinen@sci.fi>
2018-09-21 18:35:14 +0300

    Update LedStrip_v2_minimal.ino

--- LedStrip_v2_minimal.ino
+++ LedStrip_v2_minimal.ino
@@ -1,18 +1,36 @@
 #include <NeoPixelBus.h>
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(150, 2);
+uint8_t *ptr;
 
 void setup() {
   Serial.begin(2000000);
   strip.Begin();
+  ptr = strip.Pixels();
 }
 
 void loop() {
   if (Serial.available()) {
-    char *ptr = strip.Pixels();
     Serial.readBytes(ptr, 150*3);
     strip.Dirty();
     strip.Show();
   }
   delay(4);
 }
+
+/* Python:
+from PIL import Image
+import numpy as np
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 2000000)
+imgarr = np.array(Image.open("/misc/valo.png").convert("RGB"))
+
+i = 0
+while 1:
+  line = i % 300
+  ser.write(imgarr[line].flatten())
+  time.sleep(0.02)
+  i += 1
+*/
