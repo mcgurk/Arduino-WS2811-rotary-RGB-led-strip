8897df155018e1438a4efe3c9f13c65e2d23c267
Jarno Lehtinen <lehtinen@sci.fi>
2018-10-03 20:57:16 +0300

    Create LedStrip_v2_minimal_rainbow.ino

---
+++ LedStrip_v2_minimal_rainbow.ino
@@ -0,0 +1,117 @@
+// https://github.com/Makuna/NeoPixelBus
+// https://github.com/Makuna/NeoPixelBus/wiki/NeoPixelBus-object-API
+// https://github.com/Makuna/NeoPixelBus/wiki/HslColor-object-API
+
+#include <NeoPixelBus.h>
+
+#define MAX_PIXELS 300
+#define MAX_BRIGHTNESS 128
+#define SPEED 2
+#define PERIODS 2
+
+const uint8_t PixelPin = 2;
+uint16_t frame = 1;
+
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(MAX_PIXELS, PixelPin);
+
+void setup() {
+}
+
+#define HSV_HUE_SEXTANT    256
+#define HSV_HUE_STEPS   (6 * HSV_HUE_SEXTANT)
+#define HSV_HUE_MIN   0
+#define HSV_HUE_MAX   (HSV_HUE_STEPS - 1)
+#define HSV_SAT_MIN   0
+#define HSV_SAT_MAX   255
+#define HSV_VAL_MIN   0
+#define HSV_VAL_MAX   255
+
+void loop() {
+    uint16_t hue_moving = (uint16_t) (((float)(frame*SPEED))/360.0f * HSV_HUE_MAX);
+    float hue_mul = HSV_HUE_MAX/(((float)MAX_PIXELS)/PERIODS);
+    uint8_t *p = strip.Pixels();
+    for (uint16_t i = 0; i < MAX_PIXELS; i++) {
+      uint16_t hue_temp = (uint16_t)(((float)i)*hue_mul);
+      uint16_t hue = (hue_temp + hue_moving) % HSV_HUE_MAX;
+      fast_hsv2rgb_32bit(hue, 255, MAX_BRIGHTNESS, p++, p++, p++);
+    }
+    strip.Dirty();
+    strip.Show();
+  }
+  frame++;
+  if (frame == 360) frame = 1;
+}
+
+/*
+ * fast_hsv2rgb_32bit(uint16_t h, uint8_t s, uint8_t v, uint8_t *r, uint8_t *g , uint8_t *b)
+ */
+#define HSV_MONOCHROMATIC_TEST(s,v,r,g,b) \
+  do { \
+    if(!(s)) { \
+       *(r) = *(g) = *(b) = (v); \
+      return; \
+    } \
+  } while(0)
+
+
+#define HSV_SWAPPTR(a,b)  do { uint8_t *tmp = (a); (a) = (b); (b) = tmp; } while(0)
+#define HSV_POINTER_SWAP(sextant,r,g,b) \
+  do { \
+    if((sextant) & 2) { \
+      HSV_SWAPPTR((r), (b)); \
+    } \
+    if((sextant) & 4) { \
+      HSV_SWAPPTR((g), (b)); \
+    } \
+    if(!((sextant) & 6)) { \
+      if(!((sextant) & 1)) { \
+        HSV_SWAPPTR((r), (g)); \
+      } \
+    } else { \
+      if((sextant) & 1) { \
+        HSV_SWAPPTR((r), (g)); \
+      } \
+    } \
+  } while(0)
+
+void fast_hsv2rgb_32bit(uint16_t h, uint8_t s, uint8_t v, uint8_t *r, uint8_t *g , uint8_t *b) {
+  HSV_MONOCHROMATIC_TEST(s, v, r, g, b);  // Exit with grayscale if s == 0
+
+  uint8_t sextant = h >> 8;
+
+  //HSV_SEXTANT_TEST(sextant);    // Optional: Limit hue sextants to defined space
+
+  HSV_POINTER_SWAP(sextant, r, g, b); // Swap pointers depending which sextant we are in
+
+  *g = v;   // Top level
+
+  // Perform actual calculations
+
+  /*
+   * Bottom level: v * (1.0 - s)
+   * --> (v * (255 - s) + error_corr + 1) / 256
+   */
+  uint16_t ww;    // Intermediate result
+  ww = v * (255 - s); // We don't use ~s to prevent size-promotion side effects
+  ww += 1;    // Error correction
+  ww += ww >> 8;    // Error correction
+  *b = ww >> 8;
+
+  uint8_t h_fraction = h & 0xff;  // 0...255
+  uint32_t d;     // Intermediate result
+
+  if(!(sextant & 1)) {
+    // *r = ...slope_up...;
+    d = v * (uint32_t)((255 << 8) - (uint16_t)(s * (256 - h_fraction)));
+    d += d >> 8;  // Error correction
+    d += v;   // Error correction
+    *r = d >> 16;
+  } else {
+    // *r = ...slope_down...;
+    d = v * (uint32_t)((255 << 8) - (uint16_t)(s * h_fraction));
+    d += d >> 8;  // Error correction
+    d += v;   // Error correction
+    *r = d >> 16;
+  }
+  
+}
