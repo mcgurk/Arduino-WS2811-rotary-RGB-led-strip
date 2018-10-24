36f1eb8f14242ddac1fac971374d996506577571
Jarno Lehtinen <lehtinen@sci.fi>
2018-10-24 18:34:21 +0300

    Update fast_rainbow_test.ino

--- fast_rainbow_test.ino
+++ fast_rainbow_test.ino
@@ -4,17 +4,23 @@
 // http://www.vagrearg.org/content/hsvrgb
 
 #include <NeoPixelBus.h>
+//#define HSV_USE_ASSEMBLY  /* Optimize code using assembly */
+// If assembly is used, Arduino IDE must be modified!:
+// C:\Program Files (x86)\Arduino\hardware\arduino\avr\platform.txt
+// compiler.cpp.flags: -OS -> -O1
 #include "fast_hsv2rgb.c"
 
-#define MAX_PIXELS 300
-#define MAX_BRIGHTNESS 128
-#define SPEED 1
-#define PERIODS 2
+#define MAX_PIXELS 100
+#define MAX_BRIGHTNESS 20
+#define SPEED 1.0f
+#define PERIODS 2.0f
 // (100 ledstrip: 100, 255, 0.1, 1)
 // (300 ledstrip: 300, 128, 2, 2)
 
 const uint8_t PixelPin = 2;
 uint16_t frame = 0;
+float phase = 0;
+uint32_t old_t = 0;
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(MAX_PIXELS, PixelPin);
 
@@ -23,18 +29,23 @@ void setup() {
 
 void loop() {
   //float hue_moving = ((float)(frame*SPEED))/360.0f * HSV_HUE_MAX;
-  float hue_moving = ((float)(frame*SPEED))/MAX_PIXELS * HSV_HUE_MAX;
+  uint16_t hue_moving = phase * HSV_HUE_MAX;
   float hue_mul = HSV_HUE_MAX/(MAX_PIXELS/PERIODS);
   uint8_t *p = strip.Pixels();
   for (uint16_t i = 0; i < MAX_PIXELS; i++) {
-    float hue_temp = ((float)i)*hue_mul;
-    uint16_t hue = ((uint16_t)(hue_temp + hue_moving)) % HSV_HUE_MAX;
+    //float hue_temp = ((float)i)*hue_mul;
+    //uint16_t hue = ((uint16_t)(hue_temp + hue_moving)) % HSV_HUE_MAX;
+    uint16_t hue_temp = ((float)i)*hue_mul;
+    uint16_t hue = (hue_temp + hue_moving) % HSV_HUE_MAX;
     fast_hsv2rgb_32bit(hue, 255, MAX_BRIGHTNESS, p++, p++, p++);
+    //fast_hsv2rgb_8bit(hue, 255, MAX_BRIGHTNESS, p++, p++, p++);
+    //*p=i; p++; *p=i; p++; *p=i; p++;
   }
   strip.Dirty();
   strip.Show();
-  frame++;
-  //if (frame == 360) frame = 0;
-  if (frame == (MAX_PIXELS/SPEED)) frame = 0;
-}
 
+  frame++;
+  phase += SPEED/((float)(micros()-old_t));
+  old_t = micros();
+  phase = fmod(phase, 1.0f);
+}
