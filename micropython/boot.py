f29c6a7732474a5f9df96cde7023cb12b8f547a4
Jarno Lehtinen <lehtinen@sci.fi>
2019-01-08 22:53:10 +0200

    Create boot.py

---
+++ micropython/boot.py
@@ -0,0 +1,16 @@
+def reset():
+  import machine
+  machine.reset()
+
+def mem():
+  import micropython
+  micropython.mem_info()
+
+def cat(filename):
+  file = open(filename, "r")
+  while True:
+    data=file.readline()
+    if data=='':
+      break
+    print(data.rstrip("\n"))
+  file.close()
