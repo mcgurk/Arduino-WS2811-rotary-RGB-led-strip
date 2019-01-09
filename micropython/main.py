a8017d54eb3c22207b3c6595766882956279a353
Jarno Lehtinen <lehtinen@sci.fi>
2019-01-09 18:26:04 +0200

    Update main.py

--- micropython/main.py
+++ micropython/main.py
@@ -3,6 +3,18 @@ print("WS2812 LED String lightpainting system by McGurk")
 
 default_config = {'speed':4000, 'brightness':0.25, 'sleep':0.01}
 
+def hsv_to_rgb(h, s, v):
+  if s == 0.0: v*=255; return (v, v, v)
+  i = int(h*6.) # XXX assume int() truncates!
+  f = (h*6.)-i; p,q,t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f)))); v*=255; i%=6
+  v = int(v)
+  if i == 0: return (v, t, p)
+  if i == 1: return (q, v, p)
+  if i == 2: return (p, v, t)
+  if i == 3: return (p, q, v)
+  if i == 4: return (t, p, v)
+  if i == 5: return (v, p, q)
+
 def loadconfig(default = {}):
   print("default config:", default)
   try:
