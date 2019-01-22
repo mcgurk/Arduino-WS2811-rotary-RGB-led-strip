f404bcccbfb27517372037019ac07f728d51bcfe
Jarno Lehtinen <lehtinen@sci.fi>
2019-01-22 13:22:16 +0200

    Update boot.py

--- micropython/boot.py
+++ micropython/boot.py
@@ -24,5 +24,20 @@ def cat(filename):
 def delete(filename):
   file = open(filename, "w")
   file.close()
+
+def df():
+  import os
+  s = os.statvfs('//')
+  return ('{0} MB'.format((s[0]*s[3])/1048576))
+  
+# does not work with 512kB Micropython:
+def unload(mod):
+  mod_name = mod.__name__
+  import sys
+  if mod_name in sys.modules:
+    del sys.modules[mod_name]
+def run(mod):
+  unload(mod)
+  __import__(mod.__name__)
   
 # with open('boot.py','w') as f: f.write(code) '''
